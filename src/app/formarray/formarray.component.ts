f861d99e874c7156af3c6c72a19de22807be06ed
unknown <mohamedaminetarchoun@gmail.com>
2020-11-25 01:09:21 +0100

    formarray

--- src/app/formarray/formarray.component.ts
+++ src/app/formarray/formarray.component.ts
@@ -18,11 +18,11 @@ export class FormarrayComponent implements OnInit {
   registrationForm: FormGroup;
   submited: boolean = false;
   FormArray: any;
-  public users: [];
-  public user;
-  
-  
- 
+  registration: any;
+  public tab: [];
+
+
+
   constructor() {
 
   }
@@ -38,45 +38,76 @@ export class FormarrayComponent implements OnInit {
       skills: new FormArray([]),
       exppros: new FormArray([])
     });
-    
+
   }
+
+
   //  fonction submit 
-  onSubmit(){
+  onSubmit() {
     this.submited = true;
-    if(this.registrationForm.value.invalid){
+    if (this.registrationForm.value.invalid) {
       return;
     }
 
     console.log(this.registrationForm.value);
-    
+
   }
-// fonction add skills
+  // fonction add skills
   onAddskills() {
-    
+
     (<FormArray>this.registrationForm.get('skills')).push(new FormControl(''))
   }
 
   // fonction add experiences pro
-  
+
   onAddexppros() {
     (<FormArray>this.registrationForm.get('exppros')).push(new FormControl(''))
   }
 
   // fonction supprimer champ skills
-  
-  onsuppskills(index: number){
+
+  onsuppskills(index: number) {
     (<FormArray>this.registrationForm.get('skills')).removeAt(index);
-    
+
   }
   // fonction supprimer champ experience professionnelle
-  
-  onsuppexppros(index: number){
+
+  onsuppexppros(index: number) {
     (<FormArray>this.registrationForm.get('exppros')).removeAt(index);
   }
-  // fonction save les données dans le tableau
-  
-  onsavetab(index: number){
-    
-    (<FormArray><unknown>this.users).push(this.user);
+
+  // fonction save les données dans le tableau avec localstorage
+
+  onsavejson() {
+    let users = JSON.parse(localStorage.getItem('user')) || [];
+    users.push(this.registrationForm.value);
+    localStorage.setItem('user', JSON.stringify(users));
+
+    // if(localStorage.getItem('tarchoun')==null){
+    //   localStorage.setItem('tarchoun', JSON.stringify([this.registrationForm.value]));
+    // }
+
+    // else{
+    //   let taher = JSON.parse(localStorage.getItem('tarchoun'));
+    //   taher.push(this.registrationForm.value);
+    //   localStorage.setItem('tarchoun',JSON.stringify(taher));
+    // }
+
   }
-}
+
+  // Ajouter les donnes localstorage dans le TABLEAU tab
+  onsavetab(){
+    
+    tab.push(this.tab);
+    localStorage.setItem('users', JSON.stringify(this.tab));
+   
+   
+  }
+ 
+
+  } 
+   
+
+
+
+
